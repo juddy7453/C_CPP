9da34a7feaee8234db00668e7565a0e1d8b1fe74
Sein <77222048+juddy7453@users.noreply.github.com>
2023-10-29 19:11:31 +0900

    24265 : 알고리즘 수업 - 알고리즘의 수행 시간 4

---
+++ baekjoon_24265.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+long long count = 0;
+
+long long MenOfPassion(int k, int n) {
+	long long sum = 0;
+	for (int i = 1; i < n; i++) {
+		for (int j = i + 1; j <= n; j++) {
+			sum += k * k;
+			count++;
+		}
+	}
+	return sum;
+}
+
+int main(void) {
+	int n;
+
+	scanf("%d", &n);
+
+	long long sum = MenOfPassion(1, n);
+
+	printf("%lld\n", count);
+	printf("2\n");
+
+	return 0;
+}
