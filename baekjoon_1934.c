1f42568d87b6e355e35aa72e7e3d288a1dca1c0c
Sein <77222048+juddy7453@users.noreply.github.com>
2023-11-08 20:56:29 +0900

    Fix : 런타임 에러 해결
    틀렸습니다

--- baekjoon_1934.c
+++ baekjoon_1934.c
@@ -13,19 +13,27 @@ int main(void) {
 	}
 
 	// 소수 구하기
-	int prime[45000];
+	int prime[6000] = { 0, };
 	int num = 3;
 	prime[0] = 2;
-	int j = 0; int count = 1;
-	while(num<=45000) {
+	int j = 1; int count = 1;
+	while(j < 6000) {
 		int ii = 0;
 		while (ii <= j) {
 			if (ii == j) {
+				if (prime[ii] == 0) {
+					prime[count++] = num;
+					num++;
+					j++;
+					break;
+				}
 				if (num % prime[ii] != 0) {
 					prime[count++] = num;
 					num++;
+					j++;
 					break;
 				}
+
 			}
 			else if (num % prime[ii] == 0) {
 				num++;
@@ -35,24 +43,31 @@ int main(void) {
 				ii++;
 			}
 		}
-		j++;
 	}
 
 
 	int r = 0; int q = 0; j = 0;
 	for (int i = 0; i < t; i++) {
-		if (arr[i][0] == 1 || arr[i][1] == 1)
+		if (arr[i][0] == 1 || arr[i][1] == 1) // 둘 중 하나가 1일 경우
 			printf("%d\n", arr[i][0] * arr[i][1]);
-		else if (arr[i][0] % arr[i][1] == 0)
+		else if (arr[i][0] % arr[i][1] == 0) // 둘 중 하나가 다른 하나의 배수일 경우
 			printf("%d\n", arr[i][0]);
 		else if (arr[i][1] % arr[i][0] == 0)
 			printf("%d\n", arr[i][1]);
 		else {
 			q = arr[i][0] * arr[i][1];
-			while (prime[j] <= arr[i][0] || prime[j] <= arr[i][1]) {
-				if ((arr[i][0] % prime[j] == 0) && (arr[i][1] % prime[j] == 0))
+			while (prime[j] <= q) {
+				if ((arr[i][0] % prime[j] == 0) && (arr[i][1] % prime[j] == 0)) {
 					q /= prime[j];
-				j++;
+					arr[i][0] /= prime[j];
+					arr[i][1] /= prime[j];
+					continue;
+				}
+				if (prime[j+1] < 45000)
+					j++;
+				else
+					break;
+				
 			}
 			printf("%d\n", q);
 		}
