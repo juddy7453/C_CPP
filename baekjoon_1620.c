9d751b1cc6ae6820ba021fe82861ef891faabdfc
Sein <77222048+juddy7453@users.noreply.github.com>
2023-11-15 14:17:28 +0900

    1620 : 나는야 포켓몬 마스터 이다솜
    시간 초과

--- baekjoon_1620.c
+++ baekjoon_1620.c
@@ -7,6 +7,41 @@ typedef struct s {
 	char name[20];
 }Pokemon;
 
+int compare(const void* a, const void* b) {
+	if (strcmp(((Pokemon*)a)->name, ((Pokemon*)b)->name) >= 0)
+		return 1;
+	else 
+		return -1;
+}
+
+void injection(Pokemon* x, Pokemon* y) {
+	Pokemon tmp;
+
+	strcpy(tmp.name, y->name);
+	tmp.num = y->num;
+
+	strcpy(x->name, tmp.name);
+	x->num = tmp.num;
+
+}
+
+
+Pokemon bs(Pokemon*pdex, Pokemon* element, int minindex, int maxindex) {
+	int pivot = (maxindex + minindex) / 2;
+	int r = strcmp(element->name, pdex[pivot].name);
+	if (r == 0)
+		return pdex[pivot];
+	if (minindex < maxindex) {
+		if (r < 0) { // element.name이 더 위에 있는(작은) 경우
+			return bs(pdex, element, minindex, pivot-1);
+		}
+		else if (r > 0) { // element.name이 더 아래에 있는(큰) 경우
+			return bs(pdex, element, pivot + 1, maxindex);
+		}
+	}
+}
+
+
 int main(void) {
 	int n, m;
 	scanf("%d %d", &n, &m);
@@ -32,15 +67,29 @@ int main(void) {
 			qlist[i].num = 0;
 	}
 
+	Pokemon* namearr = (Pokemon*)malloc(sizeof(Pokemon) * n);
+	for (int i = 0; i < n; i++) {
+		injection(&(namearr[i]), &(pokedex[i]));
+	}
+
+	qsort(pokedex, n, sizeof(Pokemon), compare);
+
+
+	Pokemon p;
+
+
 	for (int i = 0; i < m; i++) {
 		if (qlist[i].num == 0) { // 입력이 문자인 경우 : 도감 번호 출력
-			for (int j = 0; j < n; j++) {
-				if (strcmp(pokedex[j].name, qlist[i].name) == 0)
-					printf("%d\n", pokedex[j].num);
-			}
+			p = bs(pokedex, &qlist[i], 0, n-1);
+			printf("%d\n", p.num);
 		}
 		else { // 입력이 숫자인 경우 : 포켓몬 이름 출력
-			printf("%s\n", pokedex[qlist[i].num - 1].name);
+			printf("%s\n", namearr[qlist[i].num - 1].name);
+			/*for (int j = 0; j < n; j++) {
+				if (qlist[i].num == pokedex[j].num)
+					printf("%s\n", pokedex[j].name);*/
+			
+			
 		}
 		
 	}
