79b4a671f457b60898e7b34a5a34db14c8898e83
Sein <77222048+juddy7453@users.noreply.github.com>
2023-11-20 23:00:34 +0900

    14425 : 문자열 집합

--- baekjoon_14425.c
+++ baekjoon_14425.c
@@ -2,71 +2,59 @@
 # include <stdlib.h>
 # include <string.h>
 
-int hash_func(char *arr) {
-	int h;
-	int l = 0;
-	while (1) {
-		if (arr[l] == '\0') 
-			break;
-		
-		l++;
-	}
-	int total = 0;
-	int i = 0;
-	while (i < l) {
-		total += (i+1)*(int)arr[i];
-		i++;
-	}
-	h = total;
-//	printf("h=%d\n", h);
-	return h;
+int compare(const void* a, const void* b) {
+	if (strcmp(*(char**)a, *(char**)b) > 0)
+		return 1;
+	else
+		return -1;
 }
+
+int bs(char** arr, char* element, int firstindex, int lastindex) {
+	int mid = (firstindex + lastindex) / 2;
+	int r = strcmp(*(arr + mid), element);
+
+	if (firstindex > lastindex)
+		return 0;
+
+
+
+	if (r < 0) { // element가 사전 상 더 앞에 있는 경우
+		return bs(arr, element, mid + 1, lastindex);
+	}
+	else if (r > 0) { // element가 사전 상 더 뒤에 있는 경우
+		return bs(arr, element, firstindex, mid - 1);
+	}
+	else
+		return 1;
+}
+
+
+
 
 int main(void) {
 	int n, m;
 	scanf("%d %d", &n, &m);
 
-	char (*sarr)[500];
-	char(*inputarr)[500];
-	sarr = (char(*)[500])malloc(sizeof(char) * 500 * n);
-	inputarr = (char(*)[500])malloc(sizeof(char) * 500 * m);
-	for (int i = 0; i < n; i++)
-		scanf("%s", (sarr[i]));
-	for (int i = 0; i < m; i++)
-		scanf("%s", (inputarr[i]));
-
-//	char(*r_sarr)[500];
-//	r_sarr = (char(*)[500])malloc(sizeof(char) * 500 * n);
-
-//	printf("sarr[0][10]=%c\n", sarr[0][10]);
-
-	int* hash_s = (int*)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; i++)
-		hash_s[i] = hash_func(sarr[i]);
-	int* hash_i = (int*)malloc(sizeof(int) * m);
-	for (int i = 0; i < m; i++)
-		hash_i[i] = hash_func(inputarr[i]);
-
-	//for (int i = 0; i < n; i++)
-	//	printf("i=%d, hash=%d\n", i, hash_s[i]);
-	//printf("###########\n");
-	//for (int i = 0; i < m; i++)
-	//	printf("i=%d, hash=%d\n", i, hash_i[i]);
-
-	int count = 0;
-	int t = 0;
-
-	int sp, ip;
-	for (int i = 0; i < m; i++) {
-		for (int j = 0; j < n; j++) {
-			if (hash_i[i] == hash_s[j]) {
-				count++;
-				break;
-			}
-		}
+	char** sarr = (char**)malloc(sizeof(char*) * n);
+	char** input = (char**)malloc(sizeof(char*) * m);
+	for (int i = 0; i < n; i++) {
+		*(sarr + i) = (char*)malloc(sizeof(char) * 501);
+		scanf("%s", *(sarr + i));
 	}
 
+	for (int j = 0; j < m; j++) {
+		*(input + j) = (char*)malloc(sizeof(char) * 501);
+		scanf("%s", *(input + j));
+	}
+
+	qsort(sarr, n, sizeof(char*), compare);
+
+	int count = 0;
+	for (int i = 0; i < m; i++)
+		count += bs(sarr, input[i], 0, n - 1);
+
 	printf("%d\n", count);
+
 
 	return 0;
 }
