540438f044224ace4a4ebd6049dcc53ab0f93d15
Sein <77222048+juddy7453@users.noreply.github.com>
2023-11-22 00:02:10 +0900

    Add files via upload

--- baekjoon_10816.c
+++ baekjoon_10816.c
@@ -9,50 +9,53 @@ int compare(const void* a, const void* b) {
 }
 
 int bs(int* arr, int n, int value, int startindex, int endindex) {
-	if (startindex >= endindex)
+	if (startindex > endindex)
 		return 0;
 
 	int mid = (endindex - startindex) / 2;
 	value = startindex + mid;
 	int r = arr[value] - n;
 
-	int check = 1;
+	int result = 0;
 
-	while (startindex < endindex) {
 
-		if (r < 0) { // element가 value보다 크다면
-			return bs(arr, n, value, value + 1, endindex);
+	if (r < 0) { // element가 value보다 크다면
+		return bs(arr, n, value, value + 1, endindex);
+	}
+	else if (r > 0) { // element가 value보다 작다면
+		return bs(arr, n, value, startindex, value - 1);
+	}
+	else {
+		if (value == endindex) {				
+			while (arr[value] == n) {
+				result++;
+				value--;
+			}
+			return result;
 		}
-		else if (r > 0) { // element가 value보다 작다면
-			return bs(arr, n, value, startindex, value - 1);
+		else if (value == 0) {
+			while (arr[value] == n) {
+				result++;
+				value++;
+			}
+			return result;
 		}
 		else {
-			int pivot_l = value - 1;
-			int pivot_r = value + 1;
-
-			// 오른쪽 왼쪽 중 하나라도 또 element가 있을 경우
-			if (arr[pivot_l] == n || arr[pivot_r] == n) {
-				while (arr[pivot_l] == n) {
-					pivot_l--;
-					if (pivot_l < 0) {
-						pivot_l++;
-						check = 0;
-						break;
-					}
-				}
-				while (arr[pivot_r] == n) {
-					pivot_r++;
-					if (pivot_r > endindex) {
-						pivot_r--;
-						check = 0;
-						break;
-					}
-				}
-
+			int z = 0;
+			int v = 0;
+			int tmp = value;
+			while (arr[value + z] == n) {
+				result++;
+				z++;
 			}
-			return pivot_r - pivot_l - check;
+			
 
-
+			while (arr[value - v] == n) {
+				result++;
+				v++;
+			}
+			return --result;
+		
 		}
 
 	}
@@ -95,9 +98,7 @@ int main(void) {
 			arr[i] = bs(given, have[i], 0, 0, m - 1);
 			printf("%d ", arr[i]);
 		}
-		return 0;
 	}
-
 	else if (m == 1) {
 		for (int t = 0; t < n; t++) {
 			if (given[0] == have[t])
@@ -105,8 +106,6 @@ int main(void) {
 			else
 				printf("0 ");
 		}
-		return 0;
 	}
-
 	return 0;
 }
